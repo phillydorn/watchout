6f9efc1834b4911cb51094059a2cdfe068831e3d
Phil Dornfeld <phillydorn75@gmail.com>
2015-03-17 20:55:23 -0500

    turns hero into rocket and enemies into asteroids

--- watchout.js
+++ watchout.js
@@ -13,7 +13,7 @@ function updateScore () {
   currentScoreNode.text(currentScore++);
 
 }
-setInterval(updateScore, 500);
+setInterval(updateScore, 300);
 // set the height and width of the master container
 var width = 1000;
 var height = 600;
@@ -58,31 +58,66 @@ var dragmove = function() {
   var x = d3.event.x;
   var y = d3.event.y;
   d3.select(this)
-  .attr('cx', x)
-  .attr('cy', y)
+  .attr('x', x-25)
+  .attr('y', y-25)
 }
 //drag event handler
 var drag = d3.behavior.drag()
   .on('drag', dragmove)
 
 //place the hero at the center of the board
-var hero = container.append('circle')
+
+var hero = container.append('svg')
+
+hero.attr('class', 'hero')
+    .attr('x', 0)
+    .attr('y',0)
+    .attr('height', height)
+    .attr('width', width)
+    .attr('xlink', "http://www.w3.org/1999/xlink")
+var rocket = hero.append('image')
+    .attr('class','rocket')
+    .attr('width', 50)
+    .attr('height', 50)
+    .attr('x', width/2)
+    .attr('y', height/2)
+    .attr('xlink:href', 'rocket.png')
+    .call(drag)
+   
+
+/*var hero = container.append('circle')
   .data([player])
   .attr('r', function(d) {return d.radius})
   .attr('cx', function(d) {return d.x})
   .attr('cy', function(d) {return d.y})
   .attr('fill', function(d) {return d.color})
-  .call(drag)
+  .call(drag)*/
 
 //create an array of Enemy objects 
-var numEnemies = 20;
+var numEnemies = 10;
 var enemyArray = [];
 for (var i = 0; i< numEnemies; i++) {
   enemyArray[i] = new Enemy();
 }
 
 //places all the enemies inside the container rectangle
-var enemies = container.selectAll('circle')
+
+var enemies = container.selectAll('image')
+  .data (enemyArray)
+  .enter().append('image')
+  // .attr('background-image', 'asteroid.png')
+  // .attr('width', function(d) {return d:x})
+  // .attr('height',function(d) {return d:y})
+  .attr('class', 'enemies')
+  .attr('width', 50)
+  .attr('height', 50)
+  .attr('x',function (d) {return d.x})
+  .attr('y', function (d) {return d.y})
+  .attr('xlink:href', 'asteroid.png')
+ 
+
+
+/*var enemies = container.selectAll('circle')
   .data (enemyArray)
   .enter().append('circle')
   // .attr('background-image', 'asteroid.png')
@@ -92,28 +127,28 @@ var enemies = container.selectAll('circle')
   .attr('r', function (d) {return d.radius})
   .attr('cx', function (d) {return d.x})
   .attr('cy', function(d) {return d.y})
-  .attr('fill', function(d) {return d.color})
+  .attr('fill', function(d) {return d.color})*/
  
  //tween function to track enemies paths during transition
   function makePath (d) {
     var enemy = d3.select(this);
-    var startX = enemy.attr('cx');
-    var startY = enemy.attr('cy');
+    var startX = enemy.attr('x');
+    var startY = enemy.attr('y');
     var endX = d.newX;
     var endY = d.newY;
     var interpolateX = d3.interpolate(startX, endX);
     var interpolateY = d3.interpolate(startY, endY);
     return function (t) {
-      enemy.attr('cx', interpolateX(t));
-      enemy.attr('cy', interpolateY(t));
+      enemy.attr('x', interpolateX(t));
+      enemy.attr('y', interpolateY(t));
       if (doesCollide(enemy)) {
         collisionUpdate();
       }
     }    
   }
   function doesCollide(enemy) {
-    if (Math.abs(+enemy.attr('cx') - +hero.attr('cx')) < hero.attr('r') && 
-          Math.abs(+enemy.attr('cy') - +hero.attr('cy')) < hero.attr('r')) {
+    if (Math.abs(+enemy.attr('x') - +rocket.attr('x') +rocket.attr('width')/2) < 25 && 
+          Math.abs(+enemy.attr('y') - +rocket.attr('y')+ rocket.attr('height')/2) < 25) {
       return true;
     }
     return false;
